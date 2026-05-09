c4995ec8c681b441f7a5ad5c1d3e55aa23dad114
Buse Kocinoglu <busekocinoglu@gmail.com>
2026-05-09 20:15:48 +0300

    Added secure modal for CV access with English localization

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-let menuIcon=document.querySelector("#menu-icon");
-let navbar=document.querySelector(".navbar");
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick=()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
 }
+
+// --- CV MODAL KONTROLLERİ ---
+const modal = document.getElementById("cvModal");
+const inputField = document.getElementById("cvCode");
+const errorMsg = document.getElementById("error-msg");
+
+function accessCV() {
+    modal.style.display = "block";
+    inputField.value = ""; 
+    errorMsg.style.display = "none";
+    inputField.focus(); 
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+function checkCode() {
+    const secretCode = "systembus26ceng"; 
+    
+    if (inputField.value === secretCode) {
+        window.open("buse_kocinoglu.pdf", "_blank");
+        closeModal();
+    } else {
+        errorMsg.style.display = "block";
+        inputField.style.border = "2px solid #ff4d4d"; 
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
+inputField.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        checkCode();
+    }
+});
